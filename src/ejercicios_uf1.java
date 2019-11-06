706976078d34d93a32fac8db90194f6182d63d87
Barbara <unatalbarbara@gmail.com>
2019-11-06 12:35:09 +0100

    ejercicios simples sintax

---
+++ src/ejercicios_uf1.java
@@ -0,0 +1,12 @@
+import javax.sound.midi.Soundbank;
+
+public class ejercicios_uf1 {
+    public static void main(String[] args) {
+        //Declaracion de constantes
+        final String NOMBRE = "Barbara";
+        System.out.println(NOMBRE);
+        final int IVA = 15;
+        System.out.println("El IVA es " + IVA);
+    }
+    //conditional sintax
+}
