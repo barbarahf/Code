6bcc83eee637bbe85d30db3c447f7d697c2f5fba
Barbara <unatalbarbara@gmail.com>
2019-11-08 14:49:13 +0100

    Ejemplo switch case

--- src/casting.java
+++ src/casting.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
 public class casting {
-//    public static void main(String[] arg) {
-//        Scanner hola = new Scanner(System.in);
-//        final int PRACTICA;
-//        System.out.println("Introduce un valor numerico");
-//        PRACTICA = hola.nextInt();
-//        System.out.println(PRACTICA);
-//        System.out.println("Introduce un nombre");
-//        String name = hola.nextLine();
-//        System.out.println(name);
-//    }
+    public static void main(String[] arg) {
+
+        int kotlin;
+        kotlin = (int) 5.5;
+        System.out.println(kotlin);
+        String javaScript = "el mejor lenguaje de programacion";
+        System.out.println(javaScript.length());
+        final float NOENTERO = 5.55f;
+        System.out.println(NOENTERO);
+
+        Scanner practica = new Scanner(System.in);
+        int basico;
+        System.out.println("Introduce un valor:");
+        basico = practica.nextInt();
+        System.out.println("El valor es " + basico);
+        System.out.println("introduce un string");
+        String nombres = practica.nextLine();
+        System.out.println(nombres);
+
+    }
 }
--- src/ejercicios_uf1.java
+++ src/ejercicios_uf1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ejercicios_uf1 {
     public static void main(String[] args) {
+
         //Declaracion de constantes
         final String NOMBRE = "Barbara";
         System.out.println(NOMBRE);
@@ -11,35 +12,36 @@ public class ejercicios_uf1 {
         //conditional sintax
 
         Scanner teclado = new Scanner(System.in);
+
         int numOne;
         int numTwo;
+
         System.out.print("Enter a sentence:\t");
+
         String variable2 = teclado.nextLine();
         System.out.println(variable2);
         System.out.println("Introduce un numero para cada variable");
         numOne = teclado.nextInt();
         numTwo = teclado.nextInt();
 
-        if( numOne > numTwo){
-            System.out.println("numOne " + numOne+ " es mayor");
+        if (numOne > numTwo) {
+            System.out.println("numOne " + numOne + " es mayor");
         }
-        if ( numTwo > numOne){
-            System.out.println("numTwo "+ numTwo+ " es mayor");
+        if (numTwo > numOne) {
+            System.out.println("numTwo " + numTwo + " es mayor");
         }
-        if( numOne < numTwo){
-            System.out.println("numOne " + numOne+ " es menor");
+        if (numOne < numTwo) {
+            System.out.println("numOne " + numOne + " es menor");
         }
-        if ( numTwo < numOne){
-            System.out.println("numTwo "+ numTwo+ " es menor");
-        }
-       else if (numOne == numTwo){
+        if (numTwo < numOne) {
+            System.out.println("numTwo " + numTwo + " es menor");
+        } else if (numOne == numTwo) {
             System.out.println("Son iguales");
         }
         //Num multiple of another
-        if (numOne % numTwo == 0){
+        if (numOne % numTwo == 0) {
             System.out.println("Es multiplo");
-        }
-        else {
+        } else {
             System.out.println("No es multiplo");
         }
 
@@ -52,7 +54,7 @@ public class ejercicios_uf1 {
         System.out.println("  |  ^  | ");
         System.out.println("  | '_' |");
         System.out.println("  +-----+");
-    //Scanner
+        //Scanner
 
     }
 
