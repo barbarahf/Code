b2c276c895bc6b05a6f874960a52434fb510ba20
Barbara <unatalbarbara@gmail.com>
2019-11-08 16:30:42 +0100

    Ejercicios de clase

--- src/strings.java
+++ src/strings.java
@@ -2,12 +2,36 @@ import java.util.Scanner;
 
 public class strings {
     public static void main(String[] arg) {
-//Ejercicios
+//Ejercicio 1
         Scanner practicas = new Scanner(System.in);
 //        final String NOMBRES;
         System.out.println("Introduce tu nombre");
-        String textpracticas = practicas.next(); //Acepta numeros y letras como string
+        String introduceNombre = practicas.next(); //Acepta numeros y letras como string
 //        NOMBRES = practicas.nextLine();
-        System.out.println("Hola " +"\""+ textpracticas + "\""+ " jo em dic \"programa\"");
+        System.out.println("Hola " + "\"" + introduceNombre + "\"" + " jo em dic \"programa\"");
+//Ejercicio 2
+        System.out.println("Introduce tu nombre:");
+
+        String nombre1 = practicas.next();
+
+        System.out.println("Introduce tu apellido");
+
+        String apellido1 = practicas.next();
+
+        System.out.println("Introduce tu segundo apellido");
+
+        String apellido2 = practicas.next();
+
+        String nombreCompleto = nombre1 + apellido1 + apellido2;
+
+        System.out.println(nombreCompleto);
+//Ejercicio 3
+        System.out.println("Ejercicio 3, introduce tu nombre:");
+        String stringLegth = practicas.next();
+        System.out.println(stringLegth.charAt(0));
+        System.out.println(stringLegth.charAt(stringLegth.length() - 1));
+
+        //Ejercicio4
+
     }
 }
