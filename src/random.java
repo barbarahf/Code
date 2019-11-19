5f800cfd62839c5a8107d14cebdfebee7707b84f
Barbara <unatalbarbara@gmail.com>
2019-11-20 00:47:28 +0100

    Ejercicio 4 final

--- src/random.java
+++ src/random.java
@@ -12,19 +12,22 @@ public class random {
         //Ejercicio 3
         int getNumber = (int) ((Math.random() * 6) + 1);
         Scanner input = new Scanner(System.in);
-        System.out.println("Introduce un numero entero del 1 al 6");
+        System.out.println("Enter an integer from 1 to 6");
         int userGetNum = input.nextInt();
-        System.out.println(userGetNum == getNumber ? "Yassss " + userGetNum + "  " + getNumber : "Sorry, the machine wins " + userGetNum + "  " + getNumber);
-        /*
-        public class EjemploIF {
-    public static void main(String args[]) {
-        int i = 1;
-        int j = 2;
-        int numeroMayor = 0;
-        numeroMayor = i > j ? i : j;
-        System.out.println("numeroMayor: " + numeroMayor);
-    }
-
-        * */
+        System.out.println(userGetNum == getNumber ? "You win " +"you have: "+ userGetNum + "  " +"The machine has: "+ getNumber
+        : "Sorry, the machine wins " +"you have: "+ userGetNum + "  " +"The machine has: "+ getNumber);
+       //Ejercicio 4
+        String alfabeto = "AABCDEFGHIJALMNAOPERSTUVIOAU";
+        System.out.println(alfabeto.length());
+        int longitud = (alfabeto.length()-1);
+        int caracter1 = (int) (Math.random() * (longitud+1) - 0);
+        int caracter2 = (int) (Math.random() * (longitud+1) - 0);
+        int caracter3 = (int) (Math.random() * (longitud+1) - 0);
+        int caracter4 = (int) (Math.random() * (longitud+1) - 0);
+        char result1 = alfabeto.charAt(caracter1);
+        char result2 = alfabeto.charAt(caracter2);
+        char result3 = alfabeto.charAt(caracter3);
+        char result4 = alfabeto.charAt(caracter4);
+        System.out.println( result1+" "+ result2+" "+ result3+" "+ result4);
     }
 }
