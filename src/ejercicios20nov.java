7fedc657167e9d30f4971db1aac3446871d055e8
Barbara <unatalbarbara@gmail.com>
2019-11-20 20:41:08 +0100

    ejercicios

--- src/ejercicios20nov.java
+++ src/ejercicios20nov.java
@@ -1,15 +1,20 @@
 public class ejercicios20nov {
     public static void main(String[] arg) {
-        int a = (int) 50.5;
-        int b = 150;
-        int c = (int) 500.8;
+        int a = 10;
+        int b = (int) 500.5;
+        int c = 10;
         int max;
-//        max = (a > b) ? a : b;
-//        if (a == b)
-//            a = c;  El if con una sola sentencia no necesita {
-        if (a > b && a > c) {
+        int max2 = a >= b && a >= c ?  a : b >= a && b >= c ? b : c >= b && c >= a ? c : 0;
+        System.out.println(max2);
+        int max3 = a>b && a > c ? a: b>c? b:c; //Ejemplo Faro
+        System.out.println(max3);
+         //        max = (a > b) ? a : b;
+        // if (a == b)
+        //  a = c;  El if con una sola sentencia no necesita ({)
+
+        if (a >= b && a >= c) {
             max = a;
-        } else if (b > a && b > c) {
+        } else if (b >= a && b >= c) {
             max = b;
         } else {
             max = c;
