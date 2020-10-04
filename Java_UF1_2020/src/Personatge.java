746dee6964fc3705ae3d7a371d4a3f6a92a88bc2
barbarahf <herrerabarbaraf@gmail.com>
2020-10-05 00:28:31 +0200

    final ejercicio juego2

--- Java_UF1_2020/src/Personatge.java
+++ Java_UF1_2020/src/Personatge.java
@@ -1,5 +1,5 @@
 public class Personatge {
-    private static final String[][] tablero = new String[10][10];
+    private static Object[][] tablero = new Object[10][10];
     private final String nom;
     private int x;
     private int y;
@@ -7,7 +7,7 @@ public class Personatge {
     private String mochila;
 
 
-    public Personatge(String nom, int x, int y, double presupuerto, String mochila) {
+    public Personatge(String nom, int y, int x, double presupuerto, String mochila) {
         this.nom = nom;
         this.x = x;
         this.y = y;
@@ -31,12 +31,16 @@ public class Personatge {
     }
 
     private void deixarDiners(double cuantitat, Personatge per) {
-        this.presupuerto -= cuantitat;
-        per.presupuerto += cuantitat;
+        if (checkIntercambio(per)) {
+            this.presupuerto -= cuantitat;
+            per.presupuerto += cuantitat;
+        }
     }
 
     private void agafa(Personatge p) {
-        this.mochila = p.deixa();
+        if (checkIntercambio(p)) {
+            this.mochila = p.deixa();
+        }
     }
 
     public int getX() {
@@ -48,84 +52,125 @@ public class Personatge {
     }
 
     //Move
-    private void xLeft(){
-        this.x--;
-    }
-    private void yDown(){
-        this.y--;
-    }
-    private void xRigth(){
-        this.x++;
-    }
-    private void yUp(){
-        this.y++;
+    private int xLeft() {
+        x--;
+        return x;
     }
 
-    private void move(int x){
-        while (this.x!=x){
-         if(this.x>x)  {
-             while (this.x!=x){
-                 this.xLeft();
-
-             }
-         }else{
-             this.xRigth();
-         }
-        }
-
+    private int yDown() {
+        y++;
+        return y;
     }
-    //Overload
-    private void move(int x,int y){
-        move(x);
-        while (this.y!=y){
-            if(this.y>y)  {
-                while (this.y!=y){
-                    this.yDown();
-                }
-            }else{
-                this.yUp();
+
+    private int xRigth() {
+        x++;
+        return x;
+    }
+
+    private int yUp() {
+
+        y--;
+        return y;
+    }
+
+    private void move(int newX) {//Move desplaza el objeto de forma secuencial
+        if (newX < x) {
+            while (newX < x) {
+                Object sav = tablero[y][x - 1];
+                tablero[y][x] = sav;
+                tablero[y][xLeft()] = this.getNom();
+            }
+        } else {
+            while (newX > x) {
+                Object sav = tablero[y][x + 1];
+                tablero[y][x] = sav;
+                tablero[y][xRigth()] = this.getNom();
             }
         }
-        tablero[this.x][this.y]=this.nom;
     }
 
-    //Aceptardinero()
+    //Overload
+    private void move(int newY, int newX) {
+        //No pueden estar los objectos en una misma posicion.
+        if (checkEmpty(newY, newX)) {
+            if (newX != 0) {
+                newX -= 1;
+            } else {
+                newX += 1;//Si el objeto esta en la primera posicion, desplazar el siguiente a la derecha
+            }
+        }
+        if (newY < y) {
+            while (newY < y) {
+                Object sav = tablero[y - 1][x];
+                tablero[y][x] = sav;
+                tablero[yUp()][x] = this.getNom();
+            }
+        } else {
+            while (newY > y) {
+                Object sav = tablero[y + 1][x];
+                tablero[y][x] = sav;
+                tablero[yDown()][x] = this.getNom();
+            }
+        }
+        move(newX);
+    }
 
-    //agafarCosas()
+    private boolean checkEmpty(int yCheck, int xCheck) {
+        return tablero[yCheck][xCheck] != null;
+    }
+
+    //Obtener objetos del tablero
+    private void agafarDelTablero() {
+        if (tablero[y][x + 1] instanceof String) {
+            this.mochila = tablero[y][x + 1].toString();
+        } else if (tablero[y][x - 1] instanceof String) {
+            this.mochila = tablero[y][x - 1].toString();
+        }
+    }
+
+    //Solo puden intercambiar los objetos que esten juntos
+    private boolean checkIntercambio(Personatge p) {
+        return p.x == this.x + 1 || p.x == this.x - 1;
+
+    }
+
     @Override
     public String toString() {
         return
                 "Existe un personaje de nombre '" + nom + '\'' +
-                        ", con las coordenadas (" + x + "," + y + ")" +
+                        ", con las coordenadas (" + y + "," + x + ")" +
                         ", que dispone de " + presupuerto + " euros" +
                         " y lleva " + mochila + " en la mochila" + '\'';
     }
 
     public static void main(String[] args) {
+        try {
 
-        Personatge indiana = new Personatge("Indiana", 0, 0, 50.0, "Brujula");
-        Personatge pepito = new Personatge("pepito", 7, 3, 30.0, "Mapa");
-        System.out.println(indiana);
+            Personatge indiana = new Personatge("Indiana", 0, 0, 50.0, "Brujula");
+            Personatge pepito = new Personatge("pepito", 1, 3, 30.0, "");
 
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                tablero[i][j] = " |........| ";
-            }
+            tablero[indiana.getY()][indiana.getX()] = indiana;
+            tablero[pepito.getY()][pepito.getX()] = pepito;
+            tablero[5][5] = "Mapa";
+
+            System.out.println(pepito);
+            System.out.println(indiana);
+
+            //Mover hasta encontrar a pepito
+            pepito.move(indiana.getY(), indiana.getX());//Se posiciona al lado de pepito
+            pepito.deixarDiners(5, indiana);
+            pepito.agafa(indiana);
+            //Pepito se desplaza hasta 5,5 y coge un mapa
+            pepito.move(5, 5);
+            pepito.agafarDelTablero();
+            //Utilizar getters
+            System.out.println("Nom:" + indiana.getNom());
+            System.out.println("Coordenadas:" + indiana.getY() + "," + indiana.getX());
+            System.out.println("Mochila:" + indiana.getMochila());
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        Personatge.tablero[indiana.getX()][indiana.getY()]=indiana.getNom();
-        Personatge.tablero[pepito.getX()][pepito.getY()]=pepito.getNom();
-
-        indiana.move(1,1);
-        System.out.println(indiana);
-        System.out.println("#######################################################");
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[i].length; j++) {
-                if (tablero[i][j] != null)
-                    System.out.print(tablero[i][j] + "  ");
-            }
-            System.out.println("");
-        }
-        System.out.println("#######################################################");
-
     }
 }
+
